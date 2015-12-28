cf66c29f0356fa0e6b6caeb619716bdb6c543f3a
David Goodman <deg5112@gmail.com>
2015-12-28 01:48:12 -0800

    update it was the ngAnimate injection that was messing with the bootstrap carousel

--- javascript/angularApp.js
+++ javascript/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('apartmentShark', ['ui.bootstrap', 'ngRoute', 'ngAnimate']);
+var app = angular.module('apartmentShark', ['ui.bootstrap', 'ngRoute']);
 
 app.config(function($routeProvider){
    $routeProvider

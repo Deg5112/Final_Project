052eb391b1fe98dc4489fd493ee38be0ead1d03b
David Goodman <deg5112@gmail.com>
2015-12-30 17:33:55 -0800

    update

--- javascript/apiService.js
+++ javascript/apiService.js
@@ -39,7 +39,7 @@ app.service('apiService', function($http, xmlToJsonService){
         self.getApartments = function(){
 
                 $http({
-                url: "http://52.35.28.205/Final_Project/php/getSavedApartments.php",
+                url: "http://localhost:80/Final_Project/php/getSavedApartments.php",
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                 method: 'POST',
             }).then(function(response){

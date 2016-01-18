045ca8426dc7910936479b1dc3fa774c67851859
David Goodman <deg5112@gmail.com>
2016-01-17 18:12:10 -0800

    update

--- php/zillow.php
+++ php/zillow.php
@@ -1,21 +1,21 @@
 <?php
 
-
-$zillowUrl = $_POST['url'];
-
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
-
-$result = curl_exec($curl);  //get an xml string as a response
-
-
-$responseArray = [
-    'success'=>true,
-    'data'=>$result
-];
-print(json_encode($responseArray));
+print('hello!');
+//$zillowUrl = $_POST['url'];
+//
+//$curl = curl_init();
+//
+//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
+//
+//$result = curl_exec($curl);  //get an xml string as a response
+//
+//
+//$responseArray = [
+//    'success'=>true,
+//    'data'=>$result
+//];
+//print(json_encode($responseArray));
 
 
 

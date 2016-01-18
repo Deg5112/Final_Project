0fd11302fdff3370dd90b31618278420f40dab96
David Goodman <deg5112@gmail.com>
2016-01-17 18:50:08 -0800

    update

--- php/zillow.php
+++ php/zillow.php
@@ -1,33 +1,33 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-var_dump(extension_loaded('curl'));
+//var_dump(extension_loaded('curl'));
 
-//$zillowUrl = $_POST['url'];
+$zillowUrl = $_POST['url'];
+
+
+$curl = curl_init();
+
+//
+$value = 0;
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+
+//
+
+$result = curl_exec($curl);  //get an xml string as a response
 //
 //
-//$curl = curl_init();
-//
-////
-//$value = 0;
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
-//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-//curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-//
-//
-////
-//
-//$result = curl_exec($curl);  //get an xml string as a response
-////
-////
-//
-//
-//$responseArray = [
-//    'success'=>true,
-//    'data'=>$result
-//];
-//print(json_encode($responseArray));
+
+
+$responseArray = [
+    'success'=>true,
+    'data'=>$result
+];
+print(json_encode($responseArray));
 
 
 

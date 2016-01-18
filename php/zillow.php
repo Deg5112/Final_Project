5db036a82e60b7544b47859b1d0fefec81032a93
David Goodman <deg5112@gmail.com>
2016-01-17 19:04:04 -0800

    update

--- php/zillow.php
+++ php/zillow.php
@@ -4,33 +4,29 @@ header('Access-Control-Allow-Methods: GET, POST');
 //var_dump(extension_loaded('curl'));
 
 $zillowUrl = $_POST['url'];
-$ActualzillowUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1f1y483y2ob_3l8b3&address=21551+Brookhurst+St&citystatezip=Huntington+Beach%2C+CA";
-if($zillowUrl == $ActualzillowUrl ){
-    echo 'they match';
-}else{
-    echo 'dont match!';
-}
-//$curl = curl_init();
+//$ActualzillowUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1f1y483y2ob_3l8b3&address=21551+Brookhurst+St&citystatezip=Huntington+Beach%2C+CA";
+
+$curl = curl_init();
+
 //
-////
-//$value = 0;
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
+$value = 0;
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl,  CURLOPT_URL, $zillowUrl);
+
+
+
 //
-//
-//
-////
-//
-//$result = curl_exec($curl);  //get an xml string as a response
-////
-////
+
+$result = curl_exec($curl);  //get an xml string as a response
 //
 //
-//$responseArray = [
-//    'success'=>true,
-//    'data'=>$result
-//];
-//print(json_encode($responseArray));
+
+
+$responseArray = [
+    'success'=>true,
+    'data'=>$result
+];
+print(json_encode($responseArray));
 
 
 

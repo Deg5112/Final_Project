c4b8923d9f03789b66282bb148cc9c42d19e974f
davidgoodman <dgoodman@tomferry.com>
2020-02-24 22:35:50 -0800

    file_get_contents

--- php/zillowGetPropInfo.php
+++ php/zillowGetPropInfo.php
@@ -1,28 +1,10 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-$zillowUrl = mysqli_real_escape_string($conn, $_POST['url']);
-$proxy = json_decode(file_get_contents('http://gimmeproxy.com/api/getProxy'))->curl;
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$zillowUrl);
-curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 0);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'GET');
-
-$result = curl_exec($ch);
-
-//$url = $_POST['url'];
-//$curl = curl_init();
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($curl, CURLOPT_URL, $url);
-//$result = curl_exec($curl);
 
 $responseArray = [
     'success' => true,
-    'data' => $result
+    'data' => file_get_contents($_POST['url'])
 ];
 
 print(json_encode($responseArray));

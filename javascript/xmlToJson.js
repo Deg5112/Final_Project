5c088a0cd38fe8a44f1f805722f8f63367bbdb9f
davidgoodman <dgoodman@tomferry.com>
2020-02-24 22:27:48 -0800

    xml fix

--- javascript/xmlToJson.js
+++ javascript/xmlToJson.js
@@ -1,44 +1,37 @@
 app.service('xmlToJsonService', function($q){
     var self = this;
+
     self.xmlToJson = function(xml) {
+      if (xml == null) {
+        $('#no-results').show();
+        $('#zillow-loading-gif').hide();
+      }
 
-        if (xml == null) {
-            $('#no-results').show();
-            $('#zillow-loading-gif').hide();
-        }
-        // Create the return object
+      try {
         var obj = {};
+        if (xml.children.length > 0) {
+          for (var i = 0; i < xml.children.length; i++) {
+            var item = xml.children.item(i);
+            var nodeName = item.nodeName;
 
-        if (xml.nodeType == 1) { // element
-            // do attributes
-            if (xml.attributes.length > 0) {
-                obj["@attributes"] = {};
-                for (var j = 0; j < xml.attributes.length; j++) {
-                    var attribute = xml.attributes.item(j);
-                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-                }
-            }
-        } else if (xml.nodeType == 3) { // text
-            obj = xml.nodeValue;
-        }
+            if (typeof (obj[nodeName]) == "undefined") {
+              obj[nodeName] = xml2json(item);
+            } else {
+              if (typeof (obj[nodeName].push) == "undefined") {
+                var old = obj[nodeName];
 
-        // do children
-        if (xml.hasChildNodes()) {
-            for(var i = 0; i < xml.childNodes.length; i++) {
-                var item = xml.childNodes.item(i);
-                var nodeName = item.nodeName;
-                if (typeof(obj[nodeName]) == "undefined") {
-                    obj[nodeName] = self.xmlToJson(item);
-                } else {
-                    if (typeof(obj[nodeName].push) == "undefined") {
-                        var old = obj[nodeName];
-                        obj[nodeName] = [];
-                        obj[nodeName].push(old);
-                    }
-                    obj[nodeName].push(self.xmlToJson(item));
-                }
+                obj[nodeName] = [];
+                obj[nodeName].push(old);
+              }
+              obj[nodeName].push(xml2json(item));
             }
+          }
+        } else {
+          obj = xml.textContent;
         }
         return obj;
-    };
+      } catch (e) {
+        console.log(e.message);
+      }
+  }
 });

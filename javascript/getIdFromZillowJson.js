fd41e957bea7f8560bfc979483598b2b869fc756
davidgoodman <dgoodman@tomferry.com>
2020-02-24 22:58:00 -0800

    fix logic

--- javascript/getIdFromZillowJson.js
+++ javascript/getIdFromZillowJson.js
@@ -4,21 +4,17 @@ app.service('getIdFromZillowService', function($q){
         var zpid = null;
         var result = jsonResponse['SearchResults:searchresults'];
 
-        if(result.hasOwnProperty('response')){
+        if(result.hasOwnProperty('response')) {
 
-            result = result['response']['results']['result'];
+          result = result['response']['results']['result'];
 
-            if(!(Array.isArray(result)) ){
-                zpid = result['zpid']['#text'];
-                //zillowGetPropInfo(zpid);
-                return zpid;
-            }
-
-            if( Array.isArray(result) ) {
-              return result[0]['zpid'];
-            }
-        }else{
-            zpid = null;
+          if (typeof result === 'object') {
+            return result['zpid'];
+          } else if (Array.isArray(result)) {
+            return result[0]['zpid'];
+          }
         }
+
+        return null;
     };
 });

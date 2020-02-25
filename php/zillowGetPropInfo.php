a423eddcdae62888f1968e682a222681a7caf703
davidgoodman <dgoodman@tomferry.com>
2020-02-24 22:40:51 -0800

    use zillow env

--- php/zillowGetPropInfo.php
+++ php/zillowGetPropInfo.php
@@ -2,9 +2,11 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
+$zillowKey = getenv('ZILLOW_API_KEY');
+
 $responseArray = [
     'success' => true,
-    'data' => file_get_contents($_POST['url'])
+    'data' => file_get_contents($_POST['url']."&zws-id=$zillowKey")
 ];
 
 print(json_encode($responseArray));
